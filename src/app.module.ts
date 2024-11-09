d0870133183c523b23f4d88a52959e3cd131f856
Facundo Naranjo <facundo.naranjo00@gmail.com>
2024-11-09 11:36:30 -0300

    chore: integrate TypeORM module into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MutantModule } from './modules/mutants/mutant.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { datasourceOptions } from './configuration/orm.configuration';
+import { DataSource } from 'typeorm';
 @Module({
-  imports: [MutantModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        ...datasourceOptions,
+        autoLoadEntities: true,
+        extra: {
+          timezone: 'UTC',
+        },
+      }),
+      dataSourceFactory: async (options) => {
+        return new DataSource(options).initialize();
+      },
+    }),
+    MutantModule,
+  ],
   controllers: [],
   providers: [],
 })
