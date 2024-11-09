12ea840a31967d3c986c745f5659b07432fe7743
Facundo Naranjo <facundo.naranjo00@gmail.com>
2024-11-09 12:00:53 -0300

    feat: add 'GET mutant/stats' endpoint to return DNA statistics

--- src/modules/mutants/controllers/mutant.controller.ts
+++ src/modules/mutants/controllers/mutant.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { MutantService } from '../aplication/service/mutant.service';
 import { CreateDnaDTO } from './dto/create.dna.dto';
 
 @Controller('mutant')
 export class MutantController {
   constructor(private readonly mutantService: MutantService) {}
+
+  @Get('/stats')
+  async getStats() {
+    return await this.mutantService.calculateStatistics();
+  }
 
   @Post()
   async createDna(@Body() dto: CreateDnaDTO): Promise<{
